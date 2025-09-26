de8ee28f1875723688aae997a65ad03730ba682a
lacerdaRodrigo <162244172+lacerdaRodrigo@users.noreply.github.com>
2025-09-26 20:33:41 +0000

    Criando email para report

--- pages/paginaBing/site.js
+++ pages/paginaBing/site.js
@@ -16,4 +16,18 @@ export class PaginaBing {
     await entrar.click();
     await this.page.waitForTimeout(5500); // 5.5 segundos Espera a página carregar
   }
+
+  async fazerPesquisa(nome) {
+    const inputDigitarPesquisar = await this.page.getByRole("textbox", {
+      name: "0 caracteres de 2000",
+    });
+
+    await inputDigitarPesquisar.click();
+    await inputDigitarPesquisar.type(nome, { delay: 250 }); // simula digitação humana
+  }
+
+  async clicarBotaoPesquisar() {
+    const clicarBotaoPesquisar = await this.page.locator("#search_icon svg");
+    await clicarBotaoPesquisar.click();
+  }
 }
